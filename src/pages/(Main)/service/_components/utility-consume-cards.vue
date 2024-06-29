7e20e403bc7bd20b644a7b00f3c6185f8146ae3a
puggo <puggoo1145@gmail.com>
2024-06-29 13:14:37 +0800

    完成生活服务页面 UI

--- src/pages/(Main)/service/_components/utility-consume-cards.vue
+++ src/pages/(Main)/service/_components/utility-consume-cards.vue
@@ -12,24 +12,24 @@ import images from '@/constants/images';
                 水电费余额
             </text>
             <view class="mt-3">
-                <text class="text-white font-bold text-4xl leading-none">
+                <text class="text-white font-bold text-4xl">
                     17.1
                 </text>
-                <text class="text-white text-sm">元</text>
+                <text class="text-white text-sm -ml-2">元</text>
             </view>
             <image :src="images.service.utility" class="absolute size-[110px] right-0 -bottom-[14px]" />
         </view>
         <!-- 本月在校消费 -->
         <view class="relative w-full h-[172px] bg-[#18B453] rounded-2xl
         shadow-md shadow-[#18B453] px-4 py-5">
-            <text class="text-white text-opacity-90 text-sm leading-none">
+            <text class="text-white text-opacity-90 text-sm">
                 本月在校消费
             </text>
             <view class="mt-3">
-                <text class="text-white font-bold text-4xl leading-none">
+                <text class="text-white font-bold text-4xl">
                     123
                 </text>
-                <text class="text-white text-sm">元</text>
+                <text class="text-white text-sm -ml-2">元</text>
             </view>
             <image :src="images.service.card" class="absolute size-[110px] right-0 -bottom-[14px]" />
         </view>
--- src/pages/(Main)/service/page.vue
+++ src/pages/(Main)/service/page.vue
@@ -9,16 +9,19 @@ import images from '@/constants/images';
 
 <template>
     <cus-page background-style="modern">
-        <view class="w-full px-4 flex flex-col">
+        <scroll-view 
+            class="w-full px-4"
+            scroll-y    
+        >
             <image :src="images.service.bg" class="z-0 fixed w-[255px] -right-4 top-2" />
             <view class="mt-9 w-full h-[80px]">
                 <text class="font-bold text-3xl text-modern">生活服务</text>
             </view>
-            <view class="relative z-10 flex flex-col gap-y-[14px]">
+            <view class="flex-1 relative z-10 flex flex-col gap-y-[14px]">
                 <utility-consume-cards />
                 <!-- 瓷片入口 -->
                 <cards />
             </view>
-        </view>
+        </scroll-view>
     </cus-page>
 </template>

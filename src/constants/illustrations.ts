6a4d9ca5fd766411af8aeec2745e238ae43e9af3
puggo <puggoo1145@gmail.com>
2024-08-13 21:27:16 +0800

    添加用户自定义 Image

--- src/constants/illustrations.ts
+++ src/constants/illustrations.ts
@@ -1,5 +1,20 @@
 import creating from "@/static/user-illustration/creating.svg";
+import working from "@/static/user-illustration/working.svg";
+import walking from "@/static/user-illustration/walking.svg";
+import reading from "@/static/user-illustration/reading.svg";
+import shoting from "@/static/user-illustration/shoting.svg";
+import playing from "@/static/user-illustration/playing.svg";
+import moneying from "@/static/user-illustration/moneying.svg";
+import eating from "@/static/user-illustration/eating.svg";
+
 
 export const illustrations = {
     creating,
+    working,
+    walking,
+    reading,
+    shoting,
+    playing,
+    moneying,
+    eating
 } as const;
--- src/pages/(Main)/me/_components/profile.vue
+++ src/pages/(Main)/me/_components/profile.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 // store
 import useUserInfo from '@/stores/user-info';
 // illustration
@@ -8,12 +8,42 @@ import { illustrations } from '@/constants/illustrations';
 
 const userInfo = useUserInfo();
 const userPublicInfo = computed(() => userInfo.getUserPublicInfo());
+
+
+const currentImageIndex = ref();
+function changeImage(e: any) {
+    // 存入缓存
+    uni.setStorageSync("currentImageIndex", e.detail.current);
+    currentImageIndex.value =  e.detail.current;
+}
+
+
+onMounted(() => {
+    // 从缓存读取 Image
+    const cachedImageIndex = uni.getStorageSync("currentImageIndex");
+    if (cachedImageIndex) currentImageIndex.value = cachedImageIndex;
+})
 </script>
 
 <template>
     <view class="relative w-full aspect-square bg-[#E5D7CD] 
     rounded-2xl p-6 flex flex-col overflow-hidden">
-        <image :src="illustrations.creating" class="size-full absolute left-0 top-14" />
+        <swiper 
+            class="size-full absolute left-0 top-0"
+            :current="currentImageIndex || 0"
+            @change="changeImage"
+        >
+            <swiper-item
+                v-for="(illustration, index) in illustrations"
+                :key="index"
+                class="size-full"
+            >
+                <image 
+                    :src="illustration" 
+                    class="size-full absolute left-0 top-14" 
+                />
+            </swiper-item>
+        </swiper>
         <view class="z-50 relative flex flex-col">
             <text class="text-2xl font-bold leading-[24px]">
                 {{ userPublicInfo.username }}
@@ -29,11 +59,17 @@ const userPublicInfo = computed(() => userInfo.getUserPublicInfo());
                     {{ userPublicInfo.stuClass }}
                 </text>
             </view>
+            <text
+                v-if="currentImageIndex === undefined"
+                class="text-sm text-secondary-foreground"
+            >
+                向左滑动更换我的形象
+            </text>
         </view>
     </view>
-    <view class="w-full flex flex-col gap-y-3 mt-3 bg-white p-4 rounded-2xl">
+    <!-- <view class="w-full flex flex-col gap-y-3 mt-3 bg-white p-4 rounded-2xl">
         <text class="text-sm font-bold">
             在想什么？
         </text>
-    </view>
+    </view> -->
 </template>
